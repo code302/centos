6fece864af972c556b634fd908d95cb8933512eb
code302 <38006689+code302@users.noreply.github.com>
2018-04-07 09:52:22 +0800

    Update Softether-x64.sh

--- Debian/Softether-x64.sh
+++ Debian/Softether-x64.sh
@@ -1,50 +1,69 @@
-#-------------------------------------------------------
-#Softether Auto Install Script
-#For OS Debian Ubuntu
-#
-#Script Coded by Syahz86
-#https://github.com/syahz86/SoftetherAutoInstall
-#
-#Script Based from tutorial by lincolin.hk
-#
-#Good Luck!
-#-------------------------------------------------------
-#Updating Repositories and Installing Development Packages
-apt-get update -y
-apt-get install build-essential -y
-#Get the Softether Packages via wget and Save it into /root
-cd /root
-wget http://www.softether-download.com/files/softether/v4.10-9473-beta-2014.07.12-tree/Linux/SoftEther_VPN_Server/64bit_-_Intel_x64_or_AMD64/softether-vpnserver-v4.10-9473-beta-2014.07.12-linux-x64-64bit.tar.gz
-tar zxf softether-vpnserver-v4.10-9473-beta-2014.07.12-linux-x64-64bit.tar.gz
-#Making files, manual input needed
-cd vpnserver
-clear
+nano install
+chmod 755 * && ./install
+
+#!/bin/bash
+read -r -p "This will install SoftEther to your server. Are you sure you want to continue? [y/N] " response
+case $response in
+[yY][eE][sS]|[yY])
+echo "Updating the system first..."
+apt-get update
+apt-get upgrade
+apt-get install checkinstall build-essential
+echo "Downloading last stable release: 4.20"
+sleep 2
+wget http://www.softether-download.com/files/softether/v4.25-9656-rtm-2018.01.15-tree/Linux/SoftEther_VPN_Server/64bit_-_Intel_x64_or_AMD64/softether-vpnserver-v4.25-9656-rtm-2018.01.15-linux-x64-64bit.tar.gz
+tar -xzf soft*
+cd vpn*
+echo "Please press 1 for all the following questions."
+sleep 1
 make
 cd ..
-#Moving files to /usr/local
-mv vpnserver /usr/local
-#Set the permissions
-cd /usr/local/vpnserver
-chmod 600 *
-chmod 700 vpncmd
-chmod 700 vpnserver
-#Downloading the Scripts for init.d and set Permission
-cd /root
-wget https://raw.githubusercontent.com/syahz86/SoftetherAutoInstall/master/misc/vpnserver-debian.sh --no-check-certificate
-mv vpnserver-debian.sh /etc/init.d/vpnserver
-cd /etc/init.d
-chmod 755 vpnserver
-#Creating lock and last touching...
-mkdir /var/lock/subsys
-update-rc.d vpnserver defaults
-/etc/init.d/vpnserver start
-cd /usr/local/vpnserver
-echo "-----------------------------------------------------"
-echo "Install finish!"
-echo "check this step to check are installer is working properly"
-echo "1. vpnserver and vpncmd is on /usr/local/vpnserver"
-echo -e "2. \e[1;33;44m/etc/init.d/vpnserver start\e[0m to check softether status"
-echo -e "if vpnserver started, paste \e[1;33;44mcd /usr/local/vpnserver\e[0m then \e[1;33;44m./vpncmd\e[0m for VPN configuration"
-echo "------------------------------------------------------"
-exit
+mv vpnserver /usr/local/
+chmod 600 * /usr/local/vpnserver
+chmod 700 /usr/local/vpnserver/vpncmd
+chmod 700 /usr/local/vpnserver/vpnserver
+echo '#!/bin/sh
+### BEGIN INIT INFO
+# Provides:          vpnserver
+# Required-Start:    $remote_fs $syslog
+# Required-Stop:     $remote_fs $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Start daemon at boot time
+# Description:       Enable Softether by daemon.
+### END INIT INFO
+DAEMON=/usr/local/vpnserver/vpnserver
+LOCK=/var/lock/subsys/vpnserver
+TAP_ADDR=192.168.7.1
 
+test -x $DAEMON || exit 0
+case "$1" in
+start)
+$DAEMON start
+touch $LOCK
+sleep 1
+/sbin/ifconfig tap_soft $TAP_ADDR
+;;
+stop)
+$DAEMON stop
+rm $LOCK
+;;
+restart)
+$DAEMON stop
+sleep 3
+$DAEMON start
+sleep 1
+/sbin/ifconfig tap_soft $TAP_ADDR
+;;
+*)
+echo "Usage: $0 {start|stop|restart}"
+exit 1
+esac
+exit 0' > /etc/init.d/vpnserver
+echo "System daemon created. Registering changes..."
+sleep 2
+chmod 755 /etc/init.d/vpnserver
+update-rc.d vpnserver defaults
+echo "SoftEther VPN Server should now start as a system service from now on. Starting SoftEther VPN service..."
+../etc/init.d/vpnserver start
+esac
